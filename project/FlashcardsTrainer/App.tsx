fcbde5aa2041d938914ccffe1a79a401cc2cd98d
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-27 22:36:45 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/App.tsx
+++ project/FlashcardsTrainer/App.tsx
@@ -1,37 +1,21 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.tsx to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { FlashProvider } from "./src/context/FlashContext";
-import Tabs from "./src/navigation/Tabs";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <FlashProvider>
-      <NavigationContainer>
-        <Tabs />
-      </NavigationContainer>
-    </FlashProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
