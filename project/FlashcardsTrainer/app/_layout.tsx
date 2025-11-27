ae5fb506dfb0ddd46eb0846b0f618b7e9e0464fb
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-27 22:10:45 +0000

    --- Auto Save ---

---
+++ project/FlashcardsTrainer/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { Stack } from "expo-router";
+import { FlashProvider } from "../src/contexts/FlashContext";
+
+export default function RootLayout() {
+  return (
+    <FlashProvider>
+      <Stack>
+        {/* Tabs Navigator (main app) */}
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+
+        {/* Modals */}
+        <Stack.Screen
+          name="(modals)/create-deck"
+          options={{
+            presentation: "modal",
+            title: "Create Deck",
+          }}
+        />
+        <Stack.Screen
+          name="(modals)/create-card"
+          options={{
+            presentation: "modal",
+            title: "Create Card",
+          }}
+        />
+      </Stack>
+    </FlashProvider>
+  );
+}
