8f289a0cc5706c25cfde54d95bc22fd2a6362bea
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-27 22:12:00 +0000

    --- Auto Save ---

---
+++ project/FlashcardsTrainer/app/(modals)/create-deck.tsx
@@ -0,0 +1,49 @@
+import React, {useContext, useState} from 'react' 
+import { FlashContext } from '../../src/contexts/FlashContext';
+import {useRouter} from 'expo-router'
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+export default function CreateDeckModal(){
+    const [title, setTitle] = useState("");
+    const {createDeck} = useContext(FlashContext)
+    const router = useRouter();
+
+    const handleCreate = () => {
+        if(!title.trim()) return;
+        createDeck(title.trim());
+        router.back();
+    };
+
+    return(
+        <View style={styles.container}>
+            <Text style = {styles.label}> New Deck Title</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder="Enter deck title"
+                value = {title}
+                onChangeText={setTitle}
+            />
+            <Button title="Create Deck" onPress={handleCreate}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:16,
+        justifyContent:"center",
+        backgroundColor:"#fff",
+    },
+    label:{
+        fontSize: 18,
+        marginBottom: 8,
+    },
+    input:{
+        borderWidth: 1,
+        borderColor:"#ccc",
+        borderRadius: 8,
+        padding:12,
+        marginBottom:16,
+    },
+});
