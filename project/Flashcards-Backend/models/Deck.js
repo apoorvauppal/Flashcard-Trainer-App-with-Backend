7e2db468f6189bb3575bb6bfa04205a6e8a24bee
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 19:58:57 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/models/Deck.js
+++ project/Flashcards-Backend/models/Deck.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
 const DeckSchema = new mongoose.Schema({
-    title: { type: String, required: true}
-})
+    title: { type: String, required: true},
+    createdAt: { type: Date, default: Date.now}
+});
