4eb347dc0982f0de5d5df0bb0a0b3fc9184f5875
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 20:00:27 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/models/Card.js
+++ project/Flashcards-Backend/models/Card.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 const CardSchema = new mongoose.Schema({
     deckId: { type: mongoose.Schema.Types.ObjectId, ref: "Deck", required: true},
     question: { type: String, required: true},
-    
+    answer: { type: String, required: true},
+    isFavorite
 })
