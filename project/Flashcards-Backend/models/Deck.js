b1fbf3279bdd322ac8992619c3f1f520b4118bf3
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 19:50:42 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/models/Deck.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DeckSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Deck", DeckSchema);
