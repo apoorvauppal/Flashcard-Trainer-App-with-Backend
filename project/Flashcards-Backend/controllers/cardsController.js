c3b54191d75c13d1d2e62e43f4e545656a9af526
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-29 02:41:04 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/controllers/cardsController.js
+++ project/Flashcards-Backend/controllers/cardsController.js
@@ -5,6 +5,7 @@ exports.getCardsForDeck = async(req, res) => {
     try{
         const { deckId } = req.params;
         const deck = await Deck.findById(deckId);
-        if(!deck) return res.status(404).json({ error: "Deck not found" })
+        if(!deck) return res.status(404).json({ error: "Deck not found" });
+        const cards = await Card.find({ deckId }).sort
     }
 }
