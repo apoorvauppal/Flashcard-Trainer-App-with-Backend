1c09e7b98fc15249e20931c7230090c82bc97243
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-29 03:01:35 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/routes/cardsRoutes.js
+++ project/Flashcards-Backend/routes/cardsRoutes.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const cardsController = require("../controllers/cardsController")
+const cardsController = require("../controllers/cardsController");
+
+router.put("/:cardId", cardsController)
