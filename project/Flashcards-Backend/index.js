5d6e20caec1cbf01f91ff3040f39cd5929ce5bac
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-29 03:07:05 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/index.js
+++ project/Flashcards-Backend/index.js
@@ -14,4 +14,6 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/flashcards
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+}).then(() => {
+    console.log("Connected toMongoDB");
+}).t
