cdfbb4a39eda26abee27f0d24d450be23c9e9dfc
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-29 02:55:20 +0000

    --- Auto Save ---

--- project/Flashcards-Backend/controllers/cardsController.js
+++ project/Flashcards-Backend/controllers/cardsController.js
@@ -41,6 +41,9 @@ exports.updateCard = async(req, res) => {
 
         const card = await Card.findByIdAndUpdate(cardId, updates, {new: true});
         if(!card) return res.status(404).json({ error: "Card now found"});
-        res..json(card)
+        res.json(card);
+    } catch(err){
+        console.error(err);
+        res.status(500)
     }
 }
